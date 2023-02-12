5472c9c2428d224066f789ac71244aa7d466df8b
megsummmers <70597858+megsummmers@users.noreply.github.com>
2023-02-12 15:51:45 -0500

    push title

--- Assets/Code/StartGame.cs
+++ Assets/Code/StartGame.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class StartGame : MonoBehaviour
 {
@@ -10,9 +11,7 @@ public class StartGame : MonoBehaviour
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void changeScene(){
+        SceneManager.LoadScene("HorseTraining");
     }
 }
